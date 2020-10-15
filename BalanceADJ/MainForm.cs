36fcb8fb025689d04587f7d6d308b8d657dbcd26
shematar <shematar@163.com>
2020-10-15 14:54:39 +0800

    LIY

--- BalanceADJ/MainForm.cs
+++ BalanceADJ/MainForm.cs
@@ -1039,7 +1039,7 @@ namespace BalanceADJ
             {
                 strLogLine = strLogLine.Insert(0, "OK,");
             }
-            //test
+            //tortiose
             strLogLine += strRpm + "," + strVibAgl + "," + strVibAmt + "," + strCorAgl + "," + strCormas + ",";
         }
 
